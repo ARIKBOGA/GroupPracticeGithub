2af3796c910ea34c20a0dc982175e52bf1dd9e5d
burakarikboga <barikboga42@gmail.com>
2022-06-14 03:51:02 +0300

    data.txt file's place has been re-organized

--- src/main/java/org/group/Main.java
+++ src/main/java/org/group/Main.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        String path = "src/data.txt";
+        String path = "src/main/resources/data.txt";
         new NumberExtractReport()
                 .prepareAndSendReport(path);
 
