fbbf707e5824da572f40b14ba0e7793c0cb20be6
burakarikboga <barikboga42@gmail.com>
2022-06-28 03:50:53 +0300

    Some comment has been added to the CukesRunner_Burak.java

--- src/test/java/com/meetsky/runners/CukesRunner_Burak.java
+++ src/test/java/com/meetsky/runners/CukesRunner_Burak.java
@@ -15,7 +15,8 @@ import org.junit.runner.RunWith;
                 "junit:target/junit/junit-report.xml",
                 "rerun:target/rerun.txt"
         },
-        tags = "@MTSK-372"
+        tags = "@MTSK-372"      // This must be assigned with Test Execution ticket number from JIRA in order to run all scenarios
+                                // and get their "cucumber.json" report to export to the JIRA at the end.
 )
 
 public class CukesRunner_Burak {
